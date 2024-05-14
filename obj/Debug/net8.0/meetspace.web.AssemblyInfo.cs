711e81cfcb2e73f97b072394d8864433e7378dd4
Zazouli <oussamazazouli@gmail.com>
2024-05-14 20:41:57 +0200

    add gitignore file

--- obj/Debug/net8.0/meetspace.web.AssemblyInfo.cs
+++ obj/Debug/net8.0/meetspace.web.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("meetspace.web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e351489280a58e9f22753e392a75379cdfcda5a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("meetspace.web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("meetspace.web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
